adfb8934010da6749eace33d76d4a5bf95a2e09d
Stanley Huang <stanh2019@gmail.com>
2026-08-11 14:36:55 -0500

    Added get_walk_forward_fold_predictions function to get actual predictions, not just metrics from models

--- src/models/evaluation.py
+++ src/models/evaluation.py
@@ -83,3 +83,33 @@ def run_walk_forward(df: pd.DataFrame, feature_cols: list[str], target_col: str,
         results.append(metrics)
 
     return pd.DataFrame(results)
+
+def get_walk_forward_predictions(df: pd.DataFrame, feature_cols: list[str], target_col: str, model_factory, train_size: int = 252, test_size: int = 21, step: int = None) -> pd.DataFrame:
+    # Generates folds
+    folds = walk_forward_folds(df, train_size=train_size, test_size=test_size, step=step)
+    oof_preds = []
+    
+    for train_dates, test_dates in folds:
+        """
+        Pulls all rows matching training or testing dates across all tickers
+        There are NaNs in the beginning and end, so we drop them to prevent model errors
+        """
+        train_df = df[df['date'].isin(train_dates)].dropna(subset=feature_cols + [target_col])
+        test_df = df[df['date'].isin(test_dates)].dropna(subset=feature_cols + [target_col])
+        
+        if train_df.empty or test_df.empty:
+            continue
+            
+        X_train, y_train = train_df[feature_cols], train_df[target_col]
+        X_test, y_test = test_df[feature_cols], test_df[target_col]
+        
+        model = model_factory()
+        model.fit(X_train, y_train)
+        
+        # Store test_df alongside out-of-sample predictions
+        test_pred_df = test_df[['ticker', 'date', 'log_return', target_col]].copy()
+        test_pred_df['predicted_log_vol'] = model_factory.predict(X_test)
+        oof_preds.append(test_pred_df)
+    
+    # Combines all test folds into single df
+    return(pd.concat(oof_preds, ignore_index=True).sort_values(['ticker', 'date']).reset_index(drop=True))
