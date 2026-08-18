a5223c71972844825571246d0c07d50c24a2e6ff
Stanley Huang <stanh2019@gmail.com>
2026-08-18 18:15:17 -0500

    Created session file for basic database infrastructure

---
+++ src/db/session.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environemntal variable is not set. Check the .env file.")
+
+engine = create_engine(DATABASE_URL, future=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
